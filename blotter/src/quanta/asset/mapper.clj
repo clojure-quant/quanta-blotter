e122a48b7f897ad08a6a3e67ab612eeaf5c7d3fd
awb99 <awb99@gmail.com>
2026-06-12 08:40:17 -0500

    asset mapper

---
+++ blotter/src/quanta/asset/mapper.clj
@@ -0,0 +1,10 @@
+(ns quanta.asset.mapper)
+
+
+(defprotocol asset-mapper
+  (to-api [this asset])
+  (from-api [this asset-api]))
+
+(defmulti create-asset-mapper
+  (fn [account log]
+    (:account/session account)))
